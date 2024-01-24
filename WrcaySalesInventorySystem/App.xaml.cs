3286b77b13345bf9b635e2db729862f283e79b57
issleepyhead <jomari.tenorio03@gmail.com>
2024-01-24 23:23:22 +0800

    initialize

---
+++ WrcaySalesInventorySystem/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WrcaySalesInventorySystem
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new ServiceCollection();
+            ConfigureServices(services);
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddDbContext<DBCONTEXT>(options => {
+                options.UseSqlServer("");
+            });
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void OnStartup(Object sender, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
+        }
+    }
+}
