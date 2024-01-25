143f117596efebbd420765472d8a736c96c2deaa
issleepyhead <jomari.tenorio03@gmail.com>
2024-01-25 22:24:55 +0800

    added models

---
+++ WrcaySalesInventorySystem/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrcaySalesInventorySystem.Models
+{
+    internal class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Category Name must be 50 characters or less.")]
+        [Column("CategoryName", TypeName = "VARCHAR")]
+        public string? CategoryName { get; set; }
+
+        [MaxLength(300, ErrorMessage = "Category Name must be 300 characters or less.")]
+        public string? CategoryDescription { get; set; }
+
+        public ICollection<Product>? Products { get; set; }
+    }
+}
---
+++ WrcaySalesInventorySystem/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WrcaySalesInventorySystem.Models
+{
+    internal class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Product Name must be 50 characters or less.")]
+        [Column("ProductName", TypeName = "VARCHAR")]
+        public string? ProductName { get; set; }
+
+        [Column("ProductDescription", TypeName = "VARCHAR")]
+        public string? ProductDescription { get; set; }
+
+        
+        public int CategoryID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual Category? Category { get; set; }
+    }
+}
